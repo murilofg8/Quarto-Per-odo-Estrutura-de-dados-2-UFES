22826ea968066b655d9b33d1866129e132d8cace
murilofg8 <murilofg8@gmail.com>
2026-08-10 23:28:44 -0300

    insertion Sort implementação

---
+++ InsertionSort.c
@@ -0,0 +1,22 @@
+/*Este método é usualmente utilizado para ordenar cartas de
+baralho: Considere um elemento por vez, insira cada carta em
+sua “posição correta” entre os elementos já considerados de
+forma a mantê-los ordenados.
+
+Para colocar o elemento considerado em sua posição “correta”,
+basta movimentar os elementos maiores que ele dentre aqueles
+já considerados (ordenados) uma posição para a direita e, então,
+inserir o elemento considerado na posição vaga resultante.*/
+
+void insertionSort(int A[], int n){
+    int chave, i, j;
+    for(j=1; j<n; j++){
+        chave=A[j];
+        i=j-1;
+        while(i>=0 && A[i]>chave){
+            A[i+1]=A[i];
+            i--;
+        }
+        A[i+1]=chave;
+    }
+}
