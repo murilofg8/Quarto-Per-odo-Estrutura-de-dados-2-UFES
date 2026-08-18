0d60ff7c5f4293648152f9aa648d2e3ec976f3da
murilofg8 <murilofg8@gmail.com>
2026-08-17 21:32:02 -0300

    Implementação do algoritmo Heap sort

---
+++ HeapSort.c
@@ -0,0 +1,47 @@
+void maxHeapify(int A[], int tam, int i){
+    int maior;
+    int aux;
+
+    maior = i;
+
+    if (2 * i <= tam && A[2 * i] > A[i]){
+        maior = 2 * i;
+    }
+
+    if (2 * i + 1 <= tam && A[2 * i + 1] > A[maior]){
+        maior = 2 * i + 1;
+    }
+
+    if (maior != i){
+        aux = A[i];
+        A[i] = A[maior];
+        A[maior] = aux;
+
+        maxHeapify(A, tam, maior);
+    }
+}
+
+
+void construirHeap(int A[], int tam){
+    int i;
+
+    for (i = tam / 2; i >= 1; i--){
+        maxHeapify(A, tam, i);
+    }
+}
+
+
+void heapSort(int A[], int tam){
+    int i;
+    int aux;
+
+    construirHeap(A, tam);
+
+    for (i = tam; i >= 2; i--){
+        aux = A[1];
+        A[1] = A[i];
+        A[i] = aux;
+
+        maxHeapify(A, i - 1, 1);
+    }
+}
